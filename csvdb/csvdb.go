0a872d43318558433c7e96a58ff5559b520d6513
Anil <anil@aniljava.com>
2014-01-11 19:46:44 -0600

    N/A

---
+++ csvdb/csvdb.go
@@ -0,0 +1,89 @@
+package csvdb
+
+import (
+	"encoding/csv"
+	"github.com/aniljava/goutils/generalutils"
+	"github.com/aniljava/goutils/ioutils"
+	"os"
+)
+
+type CSVDB struct {
+	Header    []string
+	file      *os.File
+	writer    *csv.Writer
+	data      [][]string
+	writemode bool
+	index     int
+}
+
+func example() {
+	db := NewWithHeader("/root/Desktop/one.csv", []string{"id", "Apple"})
+	db.Close()
+}
+
+func OpenWithHeader(name string) *CSVDB {
+	file := ioutils.CreateFile(name)
+	reader := csv.NewReader(file)
+	data, _ := reader.ReadAll()
+	file.Close()
+	db := CSVDB{
+		Header:    data[0],
+		index:     1,
+		writemode: false,
+	}
+	return &db
+}
+
+func NewWithHeader(name string, headers []string) *CSVDB {
+	file := ioutils.CreateFile(name)
+	db := CSVDB{
+		Header:    headers,
+		file:      file,
+		writer:    csv.NewWriter(file),
+		writemode: true,
+	}
+	db.Write(headers)
+	return &db
+}
+
+func (writer *CSVDB) Write(record []string) {
+	writer.writer.Write(record)
+	writer.writer.Flush()
+}
+func (writer *CSVDB) Close() {
+	writer.writer.Flush()
+	writer.file.Close()
+}
+
+func (writer *CSVDB) Next() {
+	writer.index = writer.index + 1
+}
+
+func (writer *CSVDB) HasNext() bool {
+	return len(writer.data) > writer.index
+}
+
+func (writer *CSVDB) Get(col string) string {
+	index := generalutils.ArrayIndex(writer.Header, col)
+	return writer.data[writer.index][index]
+}
+
+func (writer *CSVDB) FindRow(col string, val string) []string {
+	for _, row := range writer.data {
+		index := generalutils.ArrayIndex(writer.Header, col)
+		if index < len(row) && val == row[index] {
+			return row
+		}
+	}
+	return nil
+}
+
+func (writer *CSVDB) FindCell(searchcol string, searchval string, col string) string {
+	row := writer.FindRow(searchcol, searchval)
+
+	index := generalutils.ArrayIndex(writer.Header, col)
+	if index < len(row) {
+		return row[index]
+	}
+	return ""
+}
