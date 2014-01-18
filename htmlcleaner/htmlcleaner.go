7d7ebaffbe635906ce513ddf4dff2f0da59ecb0b
Anil <anil@aniljava.com>
2014-01-18 02:53:04 -0600

    N/A

---
+++ htmlcleaner/htmlcleaner.go
@@ -0,0 +1,77 @@
+package htmlcleaner
+
+import (
+	"code.google.com/p/go-html-transform/h5"
+	"code.google.com/p/go.net/html"
+	"github.com/aniljava/goutils/generalutils"
+	"strings"
+)
+
+var DefaultTags []string = []string{}
+var DefaultAttrs []string = []string{}
+
+func Clean(data string, tags []string, attributes []string) string {
+	if tags == nil {
+		tags = DefaultTags
+	}
+	if attributes == nil {
+		attributes = DefaultAttrs
+	}
+
+	if tree, err := h5.NewFromString(data); err == nil {
+		result := WalkNodes(tree.Top(), tags, attributes)
+		return result
+	}
+	return data
+}
+
+func WalkNodes(n *html.Node, tags, attributes []string) string {
+
+	pre, post, content := "", "", ""
+
+	if n != nil {
+		tag := n.DataAtom.String()
+		val := n.Data
+
+		include := false
+		for _, inc := range tags {
+			include = include || inc == tag
+		}
+		if !include {
+			return ""
+		}
+
+		if n.Type == 4 || (n.Type == 1 && strings.TrimSpace(val) == "") {
+			return ""
+		}
+
+		if n.Type == 1 {
+			content = generalutils.CompactTrim(n.Data)
+		}
+
+		pre = "<" + tag + ">"
+		post = "</" + tag + ">"
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if result := WalkNodes(c, tags, attributes); strings.TrimSpace(result) != "" {
+				if !strings.HasPrefix(result, "<") {
+					content += " "
+				}
+				content += result
+			}
+		}
+
+	}
+
+	if strings.TrimSpace(content) != "" {
+		result := pre + content + post
+
+		if pre == "<b>" && len(content) > 64 {
+			result = content
+		}
+
+		return result
+	} else {
+		return ""
+	}
+}
